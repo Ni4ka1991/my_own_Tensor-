7a2edfcd05408f28eee4f6c3701a5a1ed215a2ce
astahova veronica <veronika@Veronika-notebook.localdomain>
2022-04-04 10:46:43 +0300

    little change

--- app.py
+++ app.py
@@ -11,7 +11,11 @@ print( f"Tensor a( 2,3 ) >>>\n {a}" )
 print( f"Tensor a2( 3,2 ) >>>\n {a2}" )
 print( f"size of tensor a : {len(a)}" )
 print( f"size of tensor a2: {len(a2)}" )
+print( f"Shape of tensor a : {a.shape}" )
+print( f"Shape of tensor a2: {a2.shape}" )
 print( f"Tensor b([1]) >>>\n {b}" )
+print( f"size of tensor b : {len(b)}" )
+print( f"Shape of tensor b : {b.shape}" )
 print( f"Tensor c([-1]) >>>\n {c}" )
 
 
